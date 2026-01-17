17c8a1b6a94ebfd82d53907d62352b9cf80a65de
Zhanna <kezhanna.03@mail.ru>
2026-01-18 01:00:33 +0500

    task 8

--- storytellingApp/script.js
+++ storytellingApp/script.js
@@ -2,3 +2,4 @@ const storyContainer = document.querySelector(".story-container");
 const scaryStoryBtn = document.getElementById("scary-btn")
 const funnyStoryBtn = document.getElementById("funny-btn")
 const adventureStoryBtn = document.getElementById("adventure-btn")
+const resultParagraph = document.getElementById("result");
