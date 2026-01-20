d129a50515cf4412c2f770d4c1958e51b4c9ace6
Zhanna <kezhanna.03@mail.ru>
2026-01-21 00:12:18 +0500

    task 12

--- emojiReactor.js/script.js
+++ emojiReactor.js/script.js
@@ -1,16 +1,21 @@
-const happyBtn = document.querySelector("#happy-btn");
-
-happyBtn.addEventListener("click", () => {
-  updateCount(happyBtn)
-})
-const updateCount = buttonEl => {
-  const countEl = buttonEl.querySelector(".count");
+function updateCount(btn) {
+  const countEl = btn.querySelector(".count");
   let currCount = +countEl.textContent.split("/")[0];
-
+  
   if (currCount === 10) return;
-
+  
   currCount++;
-
+  
   countEl.textContent = `${currCount}/10`;
 }
+
+const happyBtn = document.querySelector("#happy-btn");
+const confusedBtn = document.querySelector("#confused-btn");
+const sadBtn = document.querySelector("#sad-btn");
+const lovingBtn = document.querySelector("#loving-btn");
+
+happyBtn.addEventListener("click", () => updateCount(happyBtn));
+confusedBtn.addEventListener("click", () => updateCount(confusedBtn));
+lovingBtn.addEventListener("click", () => updateCount(lovingBtn));
+sadBtn.addEventListener("click", () => updateCount(sadBtn));
 
