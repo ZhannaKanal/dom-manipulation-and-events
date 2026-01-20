541df97445fa79dafe43374e0c95bbe8e0dfd6db
Zhanna <kezhanna.03@mail.ru>
2026-01-20 23:59:44 +0500

    task 9

--- emojiReactor.js/script.js
+++ emojiReactor.js/script.js
@@ -2,5 +2,8 @@ const happyBtn = document.querySelector("#happy-btn");
 const count = document.querySelector(".count");
 
 happyBtn.addEventListener("click", () => {
-  console.log(count.textContent);
+  const countEl = happyBtn.querySelector(".count");
+  const currCount = +countEl.textContent.split("/")[0];
+  console.log("Current count:", currCount);
+  
 });
