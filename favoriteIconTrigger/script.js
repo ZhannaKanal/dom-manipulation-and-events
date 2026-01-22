bc0b7cd0fcf8ed8c07c648bfabf2ff0a57de8d52
Zhanna <kezhanna.03@mail.ru>
2026-01-23 00:00:06 +0500

    task 10

--- favoriteIconTrigger/script.js
+++ favoriteIconTrigger/script.js
@@ -1,5 +1,12 @@
 const favoriteIcon = document.querySelectorAll(".favorite-icon");
 
-favoriteIcon.forEach((button) => {
-  button.addEventListener("click", () => {});
-});
+favoriteIcon.forEach(
+  button => {
+    button.addEventListener("click", () => {
+      if (button.classList.contains("filled")) {
+        button.classList.remove("filled");
+        button.innerHTML = "&#9825";
+      }
+    })
+  }
+)
