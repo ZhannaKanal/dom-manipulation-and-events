bc9f87d1f664e8cb835735785d25dd2b42f7d2ce
Zhanna <kezhanna.03@mail.ru>
2026-01-25 23:45:37 +0500

    task 7

--- musicInstrumentalFilter/script.js
+++ musicInstrumentalFilter/script.js
@@ -1,7 +1,5 @@
 const instrumentsArr = [
-{
-  category: "woodwinds",
-  instrument: "Flute",
-  price: 500
-}
+  { category: "woodwinds", instrument: "Flute", price: 500 },
+  { category: "woodwinds", instrument: "Clarinet", price: 200 },
+  { category: "woodwinds", instrument: "Oboe", price: 4000 }
 ]
