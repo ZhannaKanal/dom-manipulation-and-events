e1b1dba6edfa2d6268e3d8c33b25c1e3a42c291d
Zhanna <kezhanna.03@mail.ru>
2026-01-29 23:44:00 +0500

    task 12

--- realTimeCounter/script.js
+++ realTimeCounter/script.js
@@ -3,3 +3,6 @@ const charCount = document.getElementById("char-count");
 textInput.addEventListener("input", () => {
   charCount.textContent = `Character Count: ${textInput.value.length}/50`;
 });
+if (textInput.value.length === 50) {
+  charCount.style.color = "red";
+}
