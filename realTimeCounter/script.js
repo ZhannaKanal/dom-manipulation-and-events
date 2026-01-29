422ef7df411ae065eb11fbaac8f52df6f099029a
Zhanna <kezhanna.03@mail.ru>
2026-01-29 23:32:40 +0500

    task 8

--- realTimeCounter/script.js
+++ realTimeCounter/script.js
@@ -1,4 +1,5 @@
 const textInput = document.getElementById("text-input");
 const charCount = document.getElementById("char-count");
 textInput.addEventListener("input", () => {
+  charCount.textContent = textInput.value.length;
 })
