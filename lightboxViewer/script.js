e217b5a5b9c8a9f9727c50b6d55801f28ef44d4b
Zhanna <kezhanna.03@mail.ru>
2026-01-31 03:21:31 +0500

    task 18

--- lightboxViewer/script.js
+++ lightboxViewer/script.js
@@ -1,9 +1,13 @@
 const galleryItem = document.querySelectorAll(".gallery-item");
 const lightbox = document.querySelector(".lightbox");
 const lightboxImage = document.getElementById("lightbox-image");
+const closeBtn = document.getElementById("close-btn");
 
 galleryItem.forEach((item) => {
   item.addEventListener("click", () => (lightbox.style.display = "flex"));
   const fullSrc = item.src.replace("-thumbnail", "");
   lightboxImage.src = fullSrc;
+  closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
 });
