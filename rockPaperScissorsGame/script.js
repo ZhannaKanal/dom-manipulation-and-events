7a8589a11223ddf11e4647e569ba5c205e1cdbc3
Zhanna <kezhanna.03@mail.ru>
2026-02-02 04:07:00 +0500

    task 4

---
+++ rockPaperScissorsGame/script.js
@@ -0,0 +1,21 @@
+// const rockBtn = document.getElementById("rock-btn")
+// const paperBtn = document.getElementById("paper-btn")
+// const scissorsBtn = document.getElementById("scissors-btn")
+// const arr = [
+//   "rock", "paper", "scissors"
+// ]
+
+// const computerChoice = () => {
+//   return arr[Math.floor(Math.random() * 3)]
+// }
+// let playerScore = 0;
+// rockBtn.addEventListener("click", () => {
+//   console.log(computerChoice());
+//   const computerChoice = computerChoice();
+//   if(computerChoice==="rock"){
+//     console.log("It's a tie")
+//   }else if(computerChoice==="paper"){
+//     return 
+//   }
+// })
+const options = ["Rock", "Paper", "Scissors"];
