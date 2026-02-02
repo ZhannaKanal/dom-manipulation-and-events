fdce5fb5418bc7f0cd2079fd7b06967f665ee0e2
Zhanna <kezhanna.03@mail.ru>
2026-02-02 15:45:01 +0500

    task 14

--- rockPaperScissorsGame/script.js
+++ rockPaperScissorsGame/script.js
@@ -39,6 +39,15 @@ function showResults(userOption) {
   roundResultsMsg.innerText = getRoundResults(userOption);
   computerScoreSpanElement.innerText = computerScore;
   playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
 }
 
 const rockBtn = document.getElementById("rock-btn");
