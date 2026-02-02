c7de6315a3444abd1744bfb8f37c02213a448061
Zhanna <kezhanna.03@mail.ru>
2026-02-03 02:25:41 +0500

    task 4

--- setOfFootballTeamCards/script.js
+++ setOfFootballTeamCards/script.js
@@ -2,5 +2,11 @@ const footballTeam = {
   team: "Argentina",
   year: 1986,
   headCoach: "Amigo Tanigo",
-  players: [],
+  players: [
+    {
+      name: "Diego Maradona",
+      position: "forward",
+      isCaptain: true,
+    },
+  ],
 };
