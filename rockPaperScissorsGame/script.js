22304dc546486a98efdbfce4fcb857124bd930db
Zhanna <kezhanna.03@mail.ru>
2026-02-02 15:46:33 +0500

    task 15

--- rockPaperScissorsGame/script.js
+++ rockPaperScissorsGame/script.js
@@ -50,6 +50,10 @@ function showResults(userOption) {
   }
 }
 
+resetGameBtn.addEventListener("click", () => {
+  resetGame();
+});
+
 const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
